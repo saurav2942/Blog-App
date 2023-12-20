5e2f3e3a293134781d9b4334a6e748105094a143
Saurav <91719965+saurav2942@users.noreply.github.com>
2023-12-20 16:59:23 +0530

    Update RTBlogs.js

--- src/components/RTBlogs.js
+++ src/components/RTBlogs.js
@@ -1,5 +1,34 @@
 import React from 'react'
-import Blog from './Blog';
+
+const Blog = ({ data }) => {
+  return (
+    <div className="w-11/12 max-w-2xl mx-auto">
+      <Link to={`../blog/${data?.id}`}>
+        <p className="font-bold text-lg">{data?.title}</p>
+      </Link>
+      <p className="text-sm my-1">
+        By <span className="italic">{data?.author}</span> on{" "}
+        <Link to={`../categories/${data?.category}`}>
+          <span className="font-semibold underline cursor-pointer">
+            {data?.category}
+          </span>
+        </Link>
+      </p>
+      <p className="text-sm">Posted On {data?.date}</p>
+      <p className="mt-4 mb-2">{data?.content}</p>
+      <div className="flex flex-wrap gap-x-2 items-center">
+        {data?.tags.map((e, index) => (
+          <Link to={`../tags/${e}`} key={index}>
+            <span className="text-xs font-semibold underline text-blue-700 cursor-pointer">
+              #{e}
+            </span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 
 const RTBlogs = ({blogs}) => {
   return (
